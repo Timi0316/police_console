86959c06e3a67ae08b1395fae9233713edb45596
pein000 <122024050@qq.com>
2016-03-06 23:25:43 +0800

    init police console

---
+++ console/police-service/src/main/java/LoginService.java
@@ -0,0 +1,19 @@
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by pein on 2016/3/1.
+ */
+@Service
+public class LoginService {
+
+    private Logger logger = Logger.getLogger(getClass());
+
+//    @Autowired
+//    private LoginRepository loginRepository;
+//
+//    public User login(String name, String password) {
+//        logger.info("LoginService : begin to login from LoginService");
+//        return loginRepository.login(name,password);
+//    }
+}
